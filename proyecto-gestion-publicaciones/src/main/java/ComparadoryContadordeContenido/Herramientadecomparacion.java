b93b41c2e8f71667dca6ceef81398c0ec94e05ec
csantillgar <csantill@myuax.com>
2024-04-09 21:30:14 +0200

    guardar

--- proyecto-gestion-publicaciones/src/main/java/ComparadoryContadordeContenido/Herramientadecomparacion.java
+++ proyecto-gestion-publicaciones/src/main/java/ComparadoryContadordeContenido/Herramientadecomparacion.java
@@ -1,4 +1,17 @@
 package ComparadoryContadordeContenido;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 public class Herramientadecomparacion {
+    // Método para comparar el contenido de dos archivos y determinar si son iguales
+    public boolean compararArchivos(File file1, File file2) throws IOException {
+        // Leer el contenido de ambos archivos
+        String contenido1 = new String(Files.readAllBytes(file1.toPath()));
+        String contenido2 = new String(Files.readAllBytes(file2.toPath()));
+
+        // Comparar el contenido de los archivos
+        return contenido1.equals(contenido2);
+    }
 }
