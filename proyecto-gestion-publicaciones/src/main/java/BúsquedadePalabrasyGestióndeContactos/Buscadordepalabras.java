0273185b01e9caee5ba4a92bc4171815f23cddf7
csantillgar <csantill@myuax.com>
2024-04-09 21:24:46 +0200

    guardar

--- proyecto-gestion-publicaciones/src/main/java/BúsquedadePalabrasyGestióndeContactos/Buscadordepalabras.java
+++ proyecto-gestion-publicaciones/src/main/java/BúsquedadePalabrasyGestióndeContactos/Buscadordepalabras.java
@@ -1,4 +1,21 @@
 package BúsquedadePalabrasyGestióndeContactos;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Buscadordepalabras {
+    // Método para contar la frecuencia de palabras en un texto
+    public Map<String, Integer> contarPalabras(String texto) {
+        Map<String, Integer> frecuenciaPalabras = new HashMap<>();
+
+        // Dividir el texto en palabras utilizando espacios como delimitadores
+        String[] palabras = texto.split("\\s+");
+
+        // Contar la frecuencia de cada palabra
+        for (String palabra : palabras) {
+            frecuenciaPalabras.put(palabra, frecuenciaPalabras.getOrDefault(palabra, 0) + 1);
+        }
+
+        return frecuenciaPalabras;
+    }
 }
