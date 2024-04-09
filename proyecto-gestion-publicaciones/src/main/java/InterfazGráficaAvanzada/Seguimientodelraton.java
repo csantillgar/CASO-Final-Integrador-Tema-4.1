08cf6e11369eb36800d78fa404d3fe581492e563
csantillgar <csantill@myuax.com>
2024-04-09 21:36:37 +0200

    guardar

--- proyecto-gestion-publicaciones/src/main/java/InterfazGráficaAvanzada/Seguimientodelraton.java
+++ proyecto-gestion-publicaciones/src/main/java/InterfazGráficaAvanzada/Seguimientodelraton.java
@@ -1,4 +1,43 @@
 package InterfazGráficaAvanzada;
 
-public class Seguimientodelraton {
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Seguimientodelraton extends JFrame {
+    private JLabel coordinatesLabel;
+
+    public Seguimientodelraton() {
+        setTitle("Seguimiento del Ratón");
+        setSize(300, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        coordinatesLabel = new JLabel();
+        addMouseListener(new MouseHandler());
+
+        JPanel panel = new JPanel();
+        panel.add(coordinatesLabel);
+        add(panel);
+
+        setVisible(true);
+    }
+
+    private class MouseHandler extends MouseAdapter {
+        @Override
+        public void mouseMoved(MouseEvent e) {
+            updateCoordinates(e.getX(), e.getY());
+        }
+
+        @Override
+        public void mouseDragged(MouseEvent e) {
+            updateCoordinates(e.getX(), e.getY());
+        }
+
+        private void updateCoordinates(int x, int y) {
+            coordinatesLabel.setText("Coordenadas: (" + x + ", " + y + ")");
+        }
+    }
+
+    public static void main(String[] args) {
+        new Seguimientodelraton();
+    }
 }
